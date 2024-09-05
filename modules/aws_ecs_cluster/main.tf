d2488983ffe4d95958bde39944698894cda8dfbd
Vishal-4323 <vishaldce22@gmail.com>
2024-09-05 16:42:04 +0530

    rm the cloudwatch log configuration

--- modules/aws_ecs_cluster/main.tf
+++ modules/aws_ecs_cluster/main.tf
@@ -1,11 +1,3 @@
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = var.name
-  configuration {
-    execute_command_configuration {
-      logging = "OVERRIDE"
-      log_configuration {
-        cloud_watch_log_group_name = var.cloud_watch_log_group_name
-      }
-    }
-  }
 }
